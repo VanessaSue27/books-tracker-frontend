cac0a6fadd1d883692100c70dec082f311ae6f60
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2021-03-02 16:52:11 +0100

    Add dateRead to BookCard component

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import moment from 'moment';
 
-export const BookCard = ({ name, author, imageUrl }) => {
+export const BookCard = ({ name, author, imageUrl, dateRead }) => {
   return (
     <div>
       <h3>{name}</h3>
       <h3>{author}</h3>
       <img src={imageUrl} alt="book cover" />
+      <h3>{`Finished Reading on: ${moment(dateRead).format('MMMM DD, YYYY')}`}</h3>
     </div>
   );
 };
