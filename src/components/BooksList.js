93c4413415fe3d9a4257e5ae16d8a4437c91d4fb
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2022-03-27 14:31:06 +0200

    Now fetching data form sortedBooks endpoint

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -5,19 +5,35 @@ import { BookCard } from './BookCard';
 import { BookListContainer } from '../styles/book-list-styles';
 import { AddBookTitle } from '../styles/book-form-styles';
 
-const API_URL = 'https://vane-books-tracker.herokuapp.com/books';
+// const API_URL = 'https://vane-books-tracker.herokuapp.com/books';
+const API_URL = 'http://localhost:8080/books';
+const SORTED_BOOKS_ENDPOINT = 'http://localhost:8080/sortedBooks';
+
+// Next on: remake the design so that it shows books sorted per year
+// Already getting the data sorted, so come up with new desgin to show
+// books displayed by year
 
 export const BooksList = () => {
   const [books, setBooks] = useState([]);
+  const [sortedBooks, setsortedBooks] = useState([]);
 
   useEffect(() => {
     fetch(API_URL)
       .then((res) => res.json())
       .then((json) => {
-        console.log(json);
         setBooks(json);
       })
   }, []);
+
+  useEffect(() => {
+    fetch(SORTED_BOOKS_ENDPOINT)
+      .then((res) => res.json())
+      .then((json) => {
+        setsortedBooks(json);
+      })
+  }, []);
+
+  console.log('sortedBooks', sortedBooks);
 
   return (
     <>
